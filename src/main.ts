40d6451ff0cdd5fd763567c058af70e46df7b1d8
felipelaptrin <paotrindadepao@gmail.com>
2025-11-26 09:27:45 -0300

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,7 @@
 import * as cdk from "aws-cdk-lib";
 import { WorkloadStack } from "./stack/workload";
-import { devWorkloadConfig, managementConfig } from "./config";
-import { ManagementStack } from "./stack/management";
+import { devWorkloadConfig } from "./config";
 
 const app = new cdk.App();
 
 new WorkloadStack(app, "DevWorkloadStack", { env: devWorkloadConfig.env, config: devWorkloadConfig });
-
-new ManagementStack(app, "ManagementStack", { env: managementConfig.env, config: managementConfig });
--- src/stack/management.ts
+++ src/stack/management.ts
@@ -1,6 +1,8 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { ManagementConfig } from "../config";
+import * as sso from "aws-cdk-lib/aws-sso";
+import * as customIdentityCenter from "../custom";
 
 export interface ManagementStackProps extends cdk.StackProps {
   config: ManagementConfig;
@@ -9,5 +11,42 @@ export interface ManagementStackProps extends cdk.StackProps {
 export class ManagementStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: ManagementStackProps) {
     super(scope, id, props);
+
+    const instanceArn = "arn:aws:sso:::instance/ssoins-7223c55c41650ede";
+    const applicationUrl = "https://app.demosfelipetrindade.lat";
+    const cognitoIssuerUrl = "https://auth.demosfelipetrindade.lat";
+    const cognitoUserPoolClientId = "7kej4cff5f822hlo5g70mvbaoa";
+
+    const trustTokenIssuer = new customIdentityCenter.TrustTokenIssuer(this, "TrustTokenIssuer", {
+      instanceArn: instanceArn,
+      name: "CognitoTest",
+      issuerUrl: cognitoIssuerUrl,
+    });
+
+    const identityCenterApplication = new sso.CfnApplication(this, "MyCfnApplication", {
+      applicationProviderArn: "arn:aws:sso::aws:applicationProvider/custom",
+      instanceArn: instanceArn,
+      name: "MyTestApp",
+      description: "An authless app that has authentication using Identity Center and Cognito integration.",
+      portalOptions: {
+        signInOptions: {
+          origin: "APPLICATION",
+          applicationUrl: applicationUrl,
+        },
+        visibility: "ENABLED",
+      },
+    });
+    new customIdentityCenter.ApplicationAssignmentConfiguration(this, "MyCfnApplicationAssignment", {
+      applicationArn: identityCenterApplication.attrApplicationArn,
+      assignmentRequired: false,
+    });
+    new customIdentityCenter.ApplicationGrant(this, "MyCfnApplicationGrant", {
+      applicationArn: identityCenterApplication.attrApplicationArn,
+      cognitoUserPoolClientId: cognitoUserPoolClientId,
+      trustedTokenIssuerArn: trustTokenIssuer.arn,
+    });
+    new customIdentityCenter.ApplicationAuthenticationMethod(this, "MyCfnApplicationAuthenticationMethod", {
+      applicationArn: identityCenterApplication.attrApplicationArn,
+    });
   }
 }
